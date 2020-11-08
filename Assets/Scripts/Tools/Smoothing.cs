d166f081005f43976e974fca1d8cd231fe1bdbc8
小野 寿輝 <zuginosuke@gmail.com>
2020-11-08 19:34:30 +0900

    汎用平滑化クラス追加

---
+++ Assets/Scripts/Tools/Smoothing.cs
@@ -0,0 +1,122 @@
+using System;
+using UnityEngine;
+
+namespace Unitils
+{
+	public class Smoothing
+	{
+		private readonly float[] samplingArray;
+		private readonly float[] factorArray;
+		private readonly int samplingCount;
+
+		private float smoothingFactor;
+
+		public float SmoothValue { get; private set; }
+
+		public Smoothing(int samplingCount, float currentValue)
+		{
+			this.samplingCount = Mathf.Max(samplingCount, 2);
+
+			this.samplingArray = new float[samplingCount];
+			this.factorArray = new float[this.samplingCount];
+
+			this.Reset(currentValue);
+			this.SmoothValue = currentValue;
+
+			this.SetFactor(0.5f);
+		}
+
+		public void SetFactor(float smoothingFactor)
+		{
+			this.smoothingFactor = Mathf.Clamp01(smoothingFactor);
+
+			float sum = 0f;
+			float t;
+			for (int i = 0; i < this.factorArray.Length; i++) {
+				t = (float)(i + 1) / this.factorArray.Length;
+				this.factorArray[i] = Utils.Math.SplinePoint(0f, 1f, t, this.smoothingFactor) - sum;
+				sum += this.factorArray[i];
+			}
+		}
+
+		public float Sample(float currentValue)
+		{
+			for (int i = this.samplingArray.Length - 1; i > 0; i--) {
+				this.samplingArray[i] = this.samplingArray[i - 1];
+			}
+			this.samplingArray[0] = currentValue;
+
+			this.SmoothValue = 0f;
+			for (int i = 0; i < this.samplingArray.Length; i++) {
+				this.SmoothValue += this.samplingArray[i] * this.factorArray[i];
+			}
+			return this.SmoothValue;
+		}
+
+		public void Reset(float currentValue)
+		{
+			for (int i = 0; i < this.samplingArray.Length; i++) {
+				this.samplingArray[i] = currentValue;
+			}
+		}
+	}
+
+
+	public class Smoothing<T>
+	{
+		private readonly T[] samplingArray;
+		private readonly float[] factorArray;
+		private readonly int samplingCount;
+
+		private float smoothingFactor;
+
+		private readonly Func<T[], float[], T> onSmoothMultiple;
+
+		public T SmoothValue { get; private set; }
+
+		public Smoothing(int samplingCount, T currentValue, Func<T[], float[], T> onSmoothMultiple)
+		{
+			this.samplingCount = Mathf.Max(samplingCount, 2);
+			this.onSmoothMultiple = onSmoothMultiple;
+
+			this.samplingArray = new T[samplingCount];
+			this.factorArray = new float[this.samplingCount];
+
+			this.Reset(currentValue);
+			this.SmoothValue = currentValue;
+
+			this.SetFactor(0.5f);
+		}
+
+		public void SetFactor(float smoothingFactor)
+		{
+			this.smoothingFactor = Mathf.Clamp01(smoothingFactor);
+
+			float sum = 0f;
+			float t;
+			for (int i = 0; i < this.factorArray.Length; i++) {
+				t = (float)(i + 1) / this.factorArray.Length;
+				this.factorArray[i] = Utils.Math.SplinePoint(0f, 1f, t, this.smoothingFactor) - sum;
+				sum += this.factorArray[i];
+			}
+		}
+
+		public T Sample(T currentValue)
+		{
+			for (int i = this.samplingArray.Length - 1; i > 0; i--) {
+				this.samplingArray[i] = this.samplingArray[i - 1];
+			}
+			this.samplingArray[0] = currentValue;
+
+			this.SmoothValue = this.onSmoothMultiple(this.samplingArray, this.factorArray);
+			return this.SmoothValue;
+		}
+
+		public void Reset(T currentValue)
+		{
+			for (int i = 0; i < this.samplingArray.Length; i++) {
+				this.samplingArray[i] = currentValue;
+			}
+		}
+	}
+}
--- Assets/Scripts/Utility/UtilityMath.cs
+++ Assets/Scripts/Utility/UtilityMath.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using UnityEngine;
+using System;
 
 namespace Unitils
 {
@@ -9,6 +10,38 @@ namespace Unitils
 			public static bool InRange<T>(T target, T min, T max) where T : IComparable
 			{
 				return target.CompareTo(min) >= 0 && target.CompareTo(max) <= 0;
+			}
+
+			public static float ToRoundUp(float value, int digit)
+			{
+				double dCoef = System.Math.Pow(10, digit);
+				return (float)(value > 0 ? System.Math.Ceiling(value * dCoef) / dCoef : System.Math.Floor(value * dCoef) / dCoef);
+			}
+
+			public static float ToRoundDown(float value, int digits)
+			{
+				double dCoef = System.Math.Pow(10, digits);
+				return (float)(value > 0 ? System.Math.Floor(value * dCoef) / dCoef : System.Math.Ceiling(value * dCoef) / dCoef);
+			}
+
+			public static float ToDegree(float radian)
+			{
+				return radian / Mathf.PI * 180f;
+			}
+
+			public static float ToRadian(float degree)
+			{
+				return Mathf.PI / 180f * degree;
+			}
+
+			public static float LinearPoint(float start, float end, float t)
+			{
+				return ((1f - t) * start) + (t * end);
+			}
+
+			public static float SplinePoint(float start, float end, float t, float control)
+			{
+				return (Mathf.Pow(1f - t, 2f) * start) + (2f * t * (1f - t) * control) + (Mathf.Pow(t, 2f) * end);
 			}
 		}
 	}
