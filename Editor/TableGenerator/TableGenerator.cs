7f9f8c124f34c391f4ce23694eef7325af96f377
小野 寿輝 <zuginosuke@gmail.com>
2021-02-09 00:06:36 +0900

    テーブルを統括するデータベースクラスを自動生成

--- Editor/TableGenerator/TableGenerator.cs
+++ Editor/TableGenerator/TableGenerator.cs
@@ -19,6 +19,13 @@ namespace Unitils
 			public string classNameFormat;
 		}
 
+		private class DBConfiguration
+		{
+			public string spaceName;
+			public string outputPath;
+			public List<string> tables = new List<string>();
+		}
+
 		private class SecondaryInfo
 		{
 			public bool use;
@@ -33,7 +40,10 @@ namespace Unitils
 			List<Configuration> configurations = GetConfigurations(data);
 			if (configurations.Count <= 0) return;
 
-			configurations.ForEach(_ => GenerateClass(data, _));
+			List<DBConfiguration> dBConfigurations = new List<DBConfiguration>();
+			configurations.ForEach(_ => GenerateClass(data, _, ref dBConfigurations));
+
+			GenerateDB(dBConfigurations);
 
 			AssetDatabase.SaveAssets();
 			AssetDatabase.Refresh();
@@ -90,7 +100,7 @@ namespace Unitils
 		}
 
 
-		private static void GenerateClass(TableGeneratorData data, Configuration configuration)
+		private static void GenerateClass(TableGeneratorData data, Configuration configuration, ref List<DBConfiguration> dbConfigurations)
 		{
 			if (!File.Exists(configuration.filePath)) return;
 			CSVReader csvReader = new CSVReader();
@@ -120,7 +130,7 @@ namespace Unitils
 				Debug.Log($"[TableGenerator] table name: {tableName}, primary key not found.");
 			}
 
-			string spaceName = configuration.folderName.Replace('/', '.');
+			string spaceName = configuration.folderName.Replace("/", ".");
 
 			#region Generate Class
 
@@ -183,8 +193,17 @@ namespace Unitils
 			classText = string.Format(TABLE_CLASS_TEMPLATE, spaceName, tableClassName, inheritance, classBody);
 			File.WriteAllText(Path.Combine(outputFolderPath, $"{tableClassName}.cs"), classText);
 			#endregion
-		}
 
+			DBConfiguration dbConfiguration = dbConfigurations.FirstOrDefault(_ => _.spaceName == spaceName);
+			if (dbConfiguration == null) {
+				dbConfiguration = new DBConfiguration { spaceName = spaceName, outputPath = outputFolderPath };
+				dbConfiguration.tables.Add(tableClassName);
+				dbConfigurations.Add(dbConfiguration);
+			}
+			else {
+				dbConfiguration.tables.Add(tableClassName);
+			}
+		}
 
 		private static bool IsValidateVariableType(string variableType)
 		{
@@ -233,6 +252,23 @@ namespace Unitils
 			}
 
 			return new SecondaryInfo { use = use, type = type, selector = selector, findMethod = findMethod };
+		}
+
+
+		private static void GenerateDB(List<DBConfiguration> dbConfigurations)
+		{
+			foreach (DBConfiguration dbConfiguration in dbConfigurations) {
+				string spaceName = dbConfiguration.spaceName;
+				string dbClassName = $"{spaceName.Replace(".", "")}DB";
+				string properties = "";
+
+				foreach (string table in dbConfiguration.tables) {
+					properties += string.Format(DB_CLASS_PROPERTY_TEMPLATE, table);
+				}
+
+				string classText = string.Format(DB_CLASS_TEMPLATE, spaceName, dbClassName, properties);
+				File.WriteAllText(Path.Combine(dbConfiguration.outputPath, $"{dbClassName}.cs"), classText);
+			}
 		}
 
 
@@ -338,5 +374,20 @@ namespace Unitils
 			"\t\t{{\n" +
 			"\t\t\treturn this.FindMany(this.secondaryIndex, this.secondaryIndexSelector, Comparer<{2}>.Default, key);\n" +
 			"\t\t}}\n\n";
+
+
+		private const string DB_CLASS_TEMPLATE =
+			"namespace {0}\n" +
+			"{{\n" +
+			"\tpublic partial class {1}\n" +
+			"\t{{\n" +
+			"\t\tprivate static {1} instance = null;\n" +
+			"\t\tpublic static {1} Instance => instance ??= new {1}();\n\n" +
+			"{2}" +
+			"\t}}\n" +
+			"}}";
+
+		private const string DB_CLASS_PROPERTY_TEMPLATE =
+			"\t\tpublic {0} {0} {{ get; private set; }}\n";
 	}
 }
